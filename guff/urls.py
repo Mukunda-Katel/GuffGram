5e970e655bdaf963d61c08c04eb79f363de5e1dd
Mukunda-Katel <mukundakatel@gmail.com>
2025-07-14 17:03:15 +0545

    Worked on URLs and Other setup and also worked on creating base.html to use jinja templates

--- guff/urls.py
+++ guff/urls.py
@@ -1,4 +1,20 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
+    
+    # home page
+    path('', views.home, name= 'home'),
+    
+    # Authentication URLS 
+    path('register/', views.register, name= 'register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name= 'logout'),
+    
+    # Post URLs
+    path('post/create/', views.post_create, name = 'post_create'),
+    path('post/int:post_id/edit/', views.post_edit, name='post_edit'),
+    path('post/int:post_id/delete/', views.post_delete, name='post_delete'),
+    
 ]
